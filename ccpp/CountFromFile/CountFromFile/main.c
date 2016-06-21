a1b2fcf449c122778ee41dcc925c3c8a6bada885
Ferris <fynet86@gmail.com>
2016-06-21 14:34:18 +0800

    update

---
+++ ccpp/CountFromFile/CountFromFile/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CountFromFile
+//
+//  Created by Ferris on 16/6/21.
+//  Copyright © 2016年 Ferris. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
